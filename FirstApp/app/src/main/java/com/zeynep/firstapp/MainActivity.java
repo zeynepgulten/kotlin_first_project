b22928c578e7d4e8d09dc7085027f73e68cf9222
zeynepgn <111197045+zeynepgn@users.noreply.github.com>
2022-12-07 01:36:13 +0300

    mobile
    mobile

---
+++ FirstApp/app/src/main/java/com/zeynep/firstapp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.zeynep.firstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void changeImage(View view) {
+        ImageView imageView = findViewById(R.id.ImageView);
+                imageView.setImageResource(R.drawable.lale);
+
+
+    }
+}
